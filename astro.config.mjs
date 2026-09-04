e5c27d1b1e57abf25f40ab6292a2d4347024d9c2
Norge Santana <tito.cubanito@gmail.com>
2026-09-04 18:03:56 -0300

    Configure @astrojs/vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
